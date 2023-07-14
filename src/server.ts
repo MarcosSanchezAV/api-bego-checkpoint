23777fc8c59319520919b78f2153a5060b260695
masa0922 <marcos.sanchez@netcraker.com>
2023-07-14 01:47:52 -0600

    Mejorando routes de trucks

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,12 @@ import express from "express"
 import dbConnect from "./config/mongo"
 import e from "express"
 import pointsRouter from "./routes/points.router"
+import trucksRouter from "./routes/trucks.router"
 
 const app = express() 
 const port = process.env.PORT
 
 app.use('/points', pointsRouter)
+app.use('/trucks', trucksRouter)
 dbConnect().then(() => console.log('Connection to mongoDB succesful')).catch(error => console.log('Error with mongoDB connection: ', e))
 app.listen(port, () => console.log(`Server is running in port: ${port}`))
