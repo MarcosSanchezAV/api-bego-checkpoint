22afc564a7130c9b8ef8494a179f638659fee795
masa0922 <marcos.sanchez@netcraker.com>
2023-07-13 22:59:55 -0600

    Agregando conexion a mongoDB

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import "dotenv/config"
 import express from "express"
+import dbConnect from "./config/mongo"
+import e from "express"
 
 const app = express() 
 const port = process.env.PORT
 
+dbConnect().then(() => console.log('Connection to mongoDB succesful')).catch(error => console.log('Error with mongoDB connection: ', e))
 app.listen(port, () => console.log(`Server is running in port: ${port}`))
