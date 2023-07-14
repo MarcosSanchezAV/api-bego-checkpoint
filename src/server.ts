fc9faea46320039ac6802a7877dcffe19355f0d2
masa0922 <marcos.sanchez@netcraker.com>
2023-07-13 22:29:50 -0600

    Preparando repositorio

---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import "dotenv/config"
+import express from "express"
+
+const app = express() 
+const port = process.env.PORT
+
+app.listen(port, () => console.log(`Server is running in port: ${port}`))
