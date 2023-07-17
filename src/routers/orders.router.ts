969c20e80e23f8bca42b821309c2360808875e34
masa0922 <marcos.sanchez@netcraker.com>
2023-07-16 22:19:25 -0600

    Restaurando este archivo, no debió cambiarse

--- src/routers/orders.router.ts
+++ src/routers/orders.router.ts
@@ -4,9 +4,9 @@ import { checkJwt } from "../middlewares/session.middleware";
 
 const router = Router()
 
-router.post('', addOrder) // Agregar una orden
-router.get('', getOrders) // Obtener todas las ordenes
-router.get('/:id', getOrder) //Obtener una orden
+router.post('', checkJwt, addOrder) // Agregar una orden
+router.get('', checkJwt, getOrders) // Obtener todas las ordenes
+router.get('/:id', checkJwt, getOrder) //Obtener una orden
 router.post('/:id/:status', checkJwt, changeOrderStatus) // Cambiar el estado de una orden 
 router.post('/:id', changeOrder) // Cambiar la orden 
 
